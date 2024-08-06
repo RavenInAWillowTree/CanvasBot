b620a59cf293cade558549e37f95281b7dd92423
Hyjaxaru <88588952+Hyjaxaru@users.noreply.github.com>
2024-08-06 02:31:43 +0100

    added user-specific response support to CanvasAsk

--- extensions/ask/main.py
+++ extensions/ask/main.py
@@ -15,49 +15,100 @@ with open("config.json") as f:
     default_embed_color = jdata['default_color']
 
 
-# ask a question, get a response
+# show data.json
+@ settings.register
+class SettingsShowConfig(
+    lightbulb.SlashCommand,
+    name="show-config",
+    description="display ask command json data",
+):
+    @lightbulb.invoke
+    async def invoke(self, ctx: lightbulb.Context) -> None:
+        await ctx.respond(File('extensions/ask/data.json'))
+
+
+# ask a non-user specific question, get a response
 @group.register
 class AskQuestion(
     lightbulb.SlashCommand,
     name="question",
     description="ask me anything"
 ):
-    key = lightbulb.string("key", "question key", default="showList")
+    key = lightbulb.string("key", "non-user specific response", default=None)
 
     @lightbulb.invoke
     async def invoke(self, ctx: lightbulb.Context) -> None:
-        # load json data
-        with open("extensions/ask/data.json", "r", encoding="utf-8") as f:
-            jdata = json.load(f)
+        try:
+            # load json data
+            with open("extensions/ask/data.json", "r", encoding="utf-8") as f:
+                jdata = json.load(f)
 
-        # show list of all keys if no key is given
-        if self.key == "showList":
-            body = "**Keys:**\n"
-            for item in jdata['saved'].keys():
-                body += f"{item}\n"
+            # show list of all keys if no key is given
+            if self.key is None:
+                # non-user specific
+                body = "**Keys:**\n"
+                for item in jdata['saved'].keys():
+                    body += f"{item}\n"
 
-            message = Embed(
-                title=jdata['required']['help_header'],
-                description=body,
-                color=Color.from_hex_code(default_embed_color)
-            )
-            await ctx.respond(message)
+                # user specific
+                body += "\n**Users:**\n"
+                for item in jdata['users'].keys():
+                    body += f"{item}\n"
 
-        elif self.key in jdata['saved'].keys():
-            await ctx.respond(jdata['saved'][self.key])
+                response = Embed(
+                    title=jdata['required']['help_header'],
+                    description=body,
+                    color=Color.from_hex_code(default_embed_color)
+                )
 
+            elif self.key in jdata['saved'].keys():
+                response = jdata['saved'][self.key]
+
+            else:
+                response = jdata['required']['question_key_404']
+        except Exception as e:
+            print(e)
+            await ctx.respond(jdata['required']['question_ask_failed'])
         else:
-            await ctx.respond(jdata['required']['key_404'])
+            await ctx.respond(response)
 
 
-# add or change a
-@settings.register
-class SettingsEdit(
+# ask a user specific question, get a response
+@group.register
+class AskAboutUser(
     lightbulb.SlashCommand,
-    name="edit",
-    description="edit or add a response",
+    name="about",
+    description="ask about a user"
 ):
-    key = lightbulb.string("key", "question key")
+    user = lightbulb.user("user", "user specific response")
+
+    @lightbulb.invoke
+    async def invoke(self, ctx: lightbulb.Context) -> None:
+        try:
+            # load json data
+            with open("extensions/ask/data.json", "r", encoding="utf-8") as f:
+                jdata = json.load(f)
+
+            if str(self.user) in jdata['saved'].keys():
+                response = jdata['saved'][str(self.user)]
+
+            else:
+                response = jdata['required']['user_key_404']
+        except Exception as e:
+            print(e)
+            await ctx.respond(jdata['required']['user_ask_failed'])
+        else:
+            await ctx.respond(response)
+
+
+# add or change  non-user specific response
+@settings.register
+class SettingsAddNormal(
+    lightbulb.SlashCommand,
+    name="add-normal",
+    description="add or edit a non-user specific response",
+):
+    key = lightbulb.string("key", "key for response")
     value = lightbulb.string("response", "response to key")
 
     @lightbulb.invoke
@@ -71,20 +122,22 @@ class SettingsEdit(
 
             with open("extensions/ask/data.json", "w", encoding='utf-8') as f:
                 json.dump(jdata, f, indent=4, separators=(",", ": "))
-        except:
-            await ctx.respond(jdata['required']['edit_failed'])
+        except Exception as e:
+            print(e)
+            await ctx.respond(jdata['required']['question_add_failed'])
         else:
-            await ctx.respond(jdata['required']['edit_success'])
+            await ctx.respond(jdata['required']['question_add_success'])
 
 
-# remove a response
+# add or change  non-user specific response
 @settings.register
-class SettingsRemove(
+class SettingsAddUser(
     lightbulb.SlashCommand,
-    name="remove",
-    description="remove a response",
+    name="add-user",
+    description="add or edit a user specific response",
 ):
-    key = lightbulb.string("key", "question key")
+    user = lightbulb.user("user", "key for response")
+    value = lightbulb.string("response", "response to key")
 
     @lightbulb.invoke
     async def invoke(self, ctx: lightbulb.Context) -> None:
@@ -92,23 +145,65 @@ class SettingsRemove(
             with open("extensions/ask/data.json", "r", encoding='utf-8') as f:
                 jdata = json.load(f)
 
-            del jdata['required'][self.key]
+            new_data = {str(self.user): self.value}
+            jdata['saved'].update(new_data)
 
             with open("extensions/ask/data.json", "w", encoding='utf-8') as f:
                 json.dump(jdata, f, indent=4, separators=(",", ": "))
-        except:
-            await ctx.respond(jdata['required']['remove_failed'])
+        except Exception as e:
+            print(e)
+            await ctx.respond(jdata['required']['user_add_failed'])
         else:
-            await ctx.respond(jdata['required']['remove_success'])
+            await ctx.respond(jdata['required']['user_add_success'])
 
 
-# show data.json
+# remove a non-user specific response
 @settings.register
-class SettingsShowConfig(
+class SettingsRemoveNormal(
     lightbulb.SlashCommand,
-    name="show-config",
-    description="display ask command json data",
+    name="remove-normal",
+    description="remove a non-user specific response",
 ):
+    key = lightbulb.string("key", "response key")
+
     @lightbulb.invoke
     async def invoke(self, ctx: lightbulb.Context) -> None:
-        await ctx.respond(File('extensions/ask/data.json'))
+        try:
+            with open("extensions/ask/data.json", "r", encoding='utf-8') as f:
+                jdata = json.load(f)
+
+            del jdata['saved'][self.key]
+
+            with open("extensions/ask/data.json", "w", encoding='utf-8') as f:
+                json.dump(jdata, f, indent=4, separators=(",", ": "))
+        except Exception as e:
+            print(e)
+            await ctx.respond(jdata['required']['question_remove_failed'])
+        else:
+            await ctx.respond(jdata['required']['question_remove_success'])
+
+
+# remove a non-user specific response
+@settings.register
+class SettingsRemoveUser(
+    lightbulb.SlashCommand,
+    name="remove-user",
+    description="remove a user specific response",
+):
+    user = lightbulb.user("user", "response key")
+
+    @lightbulb.invoke
+    async def invoke(self, ctx: lightbulb.Context) -> None:
+        try:
+            with open("extensions/ask/data.json", "r", encoding='utf-8') as f:
+                jdata = json.load(f)
+
+            del jdata['saved'][str(self.user)]
+
+            with open("extensions/ask/data.json", "w", encoding='utf-8') as f:
+                json.dump(jdata, f, indent=4, separators=(",", ": "))
+        except Exception as e:
+            print(e)
+            await ctx.respond(jdata['required']['user_remove_failed'])
+        else:
+            await ctx.respond(jdata['required']['user_remove_success'])
